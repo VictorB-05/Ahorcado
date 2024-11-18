d7594f70303ff0da480574b06b6b7a2e941c38aa
victor <>
2024-11-18 21:02:40 +0100

    Empezamos con las tablas de las bases de datos

--- database.py
+++ database.py
@@ -5,3 +5,19 @@ conn = sqlite3.connect('Registro.db')
 # Crear un cursor
 cursor = conn.cursor()
 
+cursor.execute("IF NOT EXIST CREATE table jugador"
+               "(id INTEGER PRIMARY KEY AUTOINCREMENT,"
+               "nombre TEXT)")
+
+cursor.execute("IF NOT EXIST CREATE table palabra"
+               "(id INTEGER PRIMARY KEY AUTOINCREMENT,"
+               "palabra TEXT,"
+               "tipo TEXT)")
+
+cursor.execute("IF NOT EXIST CREATE table partida"
+               "(id INTEGER PRIMARY KEY AUTOINCREMENT,"
+               "ganada BOOLEAN,"
+               "idJugador INTEGER,"
+               "idPalabra INTEGER,"
+               "FOREIGN KEY(idJugador) REFERENCES jugador(id),"
+               "FOREIGN KEY(idPalabra) REFERENCES palabra(id))")
